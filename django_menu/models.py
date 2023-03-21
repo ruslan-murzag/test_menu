a68ba5173783a50c207482488c430422fd6004fd
ruslan <rus.mur2001@gmail.com>
2023-03-21 17:23:04 +0600

    fix

--- django_menu/models.py
+++ django_menu/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255, blank=True)
-    parent = models.ForeignKey('self',  null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name='название')
+    url = models.CharField(max_length=255, blank=True, verbose_name='ссылка')
+    parent = models.ForeignKey('self', verbose_name='родитель',  null=True, blank=True, related_name='children', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
 class Menu(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='название')
     items = models.ManyToManyField(MenuItem)
 
     def __str__(self):
